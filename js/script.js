85507319a1fff75b32e87ff098be026e7e7a9d8c
Nourhan-magdy <129990041+Nourhan-magdy@users.noreply.github.com>
2023-11-03 10:46:38 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,48 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav= document.getElementById('navbar');
+
+if (bar){
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close){
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+var content = document.getElementsByTagName('body')[0];
+var darkMode = document.getElementById('dark-change');
+darkMode.addEventListener('click', function(){
+    darkMode.classList.toggle('active');
+    content.classList.toggle('night');
+})
